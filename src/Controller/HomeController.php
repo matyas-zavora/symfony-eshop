99b9c97fdef263f281fd3f65031667a45d02111c
SonyaBo <95350503+SonyaBo@users.noreply.github.com>
2024-02-16 11:43:40 +0100

    Vsechno co jsem stihla

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Získání všech produktů z databáze
+        $products = $entityManager->getRepository(Product::class)->findAll();
+
+        // Předání dat do Twig šablony
         return $this->render('home.html.twig', [
-            'controller_name' => 'HomeController',
+            'products' => $products,
         ]);
     }
 }
